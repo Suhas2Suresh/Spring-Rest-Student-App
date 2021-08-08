695ff60af0935eb63ba1cb32f5c27420308fedc1
Suhas S <suhaspushpa66@gmail.com>
2021-08-08 14:38:56 +0530

    Moved the service impl file to service folder

---
+++ src/main/java/com/app/service/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,55 @@
+package com.app.service.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.entity.Student;
+import com.app.repository.StudentRepository;
+import com.app.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+	
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Override
+	public Student createStudent(Student student)  {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudent(int id)  {
+		studentRepository.deleteById(id);;
+	}
+
+	@Override
+	public Student updateStudent(Student student)  {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.NEVER)
+	public List<Student> getAllStudents()  {
+		
+		return (List<Student>) studentRepository.findAll();
+	}
+
+	@Override
+	public Boolean isStudentRecordExists(int id) {
+
+		return studentRepository.existsById(id);
+	}
+
+	@Override
+	public Optional<Student> findStudentById(int id) {
+		
+		return studentRepository.findById(id);
+	}
+
+}
